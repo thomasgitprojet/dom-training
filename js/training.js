efab7202c189d44d175d00e458795e91926c0cbd
caravane <thomasdesse.pro@gmail.com>
2024-04-15 16:03:43 +0200

    ex3

--- js/training.js
+++ js/training.js
@@ -1,14 +1,30 @@
 /* ------------------------------------ */
 /* --- Exercice 1 --- */
+const btn = document.querySelector('#ex1 button').addEventListener("click", function(event) {
+    const text = document.querySelector("#ex1 .text");
+    text.classList.toggle('hidden')
+});
 
 
 /* ------------------------------------ */
 /* --- Exercice 2 --- */
 
+document.addEventListener("scroll",function (event) {
+    scrollPosition = window.scrollY;
+    console.log(scrollPosition);
+    const positionScroll = document.querySelector("#ex2-scroll-value");
+    console.log(positionScroll);
+    positionScroll.innerHTML = scrollPosition;
+})
+
 
 /* ------------------------------------ */
 /* --- Exercice 3 --- */
-
+ document.querySelector("#btn3")
+ .addEventListener("click", function (event) {
+    let firstanimal = document.querySelector("#ex3-animals li");
+    document.querySelector("#ex3-animals").appendChild(firstanimal)
+});
 
 /* ------------------------------------ */
 /* --- Exercice 4 --- */
