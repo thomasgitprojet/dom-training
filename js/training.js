44f70e46b35517d138134e4016c88f9a23612960
caravane <thomasdesse.pro@gmail.com>
2024-04-17 09:01:26 +0200

    ex6

--- js/training.js
+++ js/training.js
@@ -10,11 +10,8 @@ const btn = document.querySelector('#ex1 button').addEventListener("click", func
 /* --- Exercice 2 --- */
 
 document.addEventListener("scroll",function (event) {
-    scrollPosition = window.scrollY;
-    console.log(scrollPosition);
     const positionScroll = document.querySelector("#ex2-scroll-value");
-    console.log(positionScroll);
-    positionScroll.innerHTML = scrollPosition;
+    positionScroll.textContent = Math.floor(window.scrollY);
 })
 
 
@@ -22,20 +19,67 @@ document.addEventListener("scroll",function (event) {
 /* --- Exercice 3 --- */
  document.querySelector("#btn3")
  .addEventListener("click", function (event) {
-    let firstanimal = document.querySelector("#ex3-animals li");
-    document.querySelector("#ex3-animals").appendChild(firstanimal)
+    document.querySelector("#ex3-animals").appendChild(document.querySelector("#ex3-animals li"))
 });
 
 /* ------------------------------------ */
 /* --- Exercice 4 --- */
-
+let count = 0;
+document.querySelector("#ex4 .button").addEventListener("click", function (event) {
+        this.classList.add("blue");
+        count++;
+    console.log(document.querySelector("#ex4 .button").className);
+    console.log(count);
+    if(count === 2) {
+        this.classList.add("red");
+    }
+    if(count === 3) {
+        this.classList.add("green")
+    }
+    if (count > 3) {
+        this.classList.remove("red", "green");
+        count = 0;
+    }
+})
 
 /* ------------------------------------ */
 /* --- Exercice 5 --- */
+function random_bg_color(event) {
+    // Generate random values for red, green, and blue components between 0 and 255.
+    let x = Math.floor(Math.random() * 256);
+    let y = Math.floor(Math.random() * 256);
+    let z = Math.floor(Math.random() * 256);
+    // Construct the RGB color string.
+    let bgColor = "rgb(" + x + "," + y + "," + z + ")";
+    // Output the generated color to the console.
+    // console.log(bgColor);
+    event.target.style.background = bgColor;;
+}
+
+
+for(let btn of document.querySelectorAll("#ex5 button")) {
+    btn.addEventListener("mouseover", function (event) {    
+    random_bg_color(event);
+    })
+}
 
 
 /* ------------------------------------ */
 /* --- Exercice 6 --- */
+
+// const text = document.querySelector("#ex6 .exercice-txt");
+// console.log(text);
+const text = document.querySelector("#ex6-paragraph").innerHTML.trim();
+console.log(text);
+const letters = text.split("");
+console.log(letters);
+document.querySelector("#ex6-paragraph").innerHTML = "";
+
+
+letters.forEach((letter, i) => {
+    setTimeout(() => document.querySelector("#ex6-paragraph").innerHTML += letter, i * 50)
+});
+
 
 
 
