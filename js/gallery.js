8e7e49120955af2f0bb5a5896369aa7b3bbd50aa
Thomas Desse <dessethomas@gmail.com>
2024-04-18 09:20:15 +0200

    gallery 2.

--- js/gallery.js
+++ js/gallery.js
@@ -1,26 +1,27 @@
-const imgs = document.querySelectorAll(".thumbs-img")
-console.log(imgs);
+const imgs = document.querySelectorAll(".thumbs-img");
+const bigImg = document.querySelector("#gallery-picture");
+const titleText = document.querySelector("#gallery-title");
+const descriptionImg = document.querySelector("#gallery-description");
 
-const bigImg = document.querySelector("#gallery-picture")
-console.log(bigImg);
-
-// imgs[i].addEventListener("mouseover", function(e) {
-    
-// })
-
-
-
-function displayImg (title, description, src) {
-    bigImg.setAttribute("src", src)
+/**
+ * Display image, title and description when the miniatur is over
+ * @param {string} title title of image
+ * @param {string} description description of image
+ * @param {string} src url of image
+ */
+function displayImgTltDescp (title, description, src) {
+    bigImg.setAttribute("src", src);
+    titleText.textContent = title;
+    descriptionImg.textContent = description;
 }
 
 for (const img of imgs) {
     img.addEventListener("mouseover", function(e) {
-        let dataTitle = img.dataset.title
-        let dataDescription = img.dataset.description
-        let src = img.currentSrc
+        let dataTitle = img.dataset.title;
+        let dataDescription = img.dataset.description;
+        let src = img.currentSrc;
         // console.log(src);
-        displayImg(dataTitle, dataDescription, src)
+        displayImgTltDescp(dataTitle, dataDescription, src);
     })  
 }
 
