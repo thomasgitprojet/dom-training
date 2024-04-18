519dfde4287bc7ea3d46c8ff95d7b4f63d485a15
Thomas Desse <dessethomas@gmail.com>
2024-04-18 16:20:22 +0200

    salade 1

--- js/salad.js
+++ js/salad.js
@@ -1,2 +1,14 @@
 
 const ingredients = ["🍅", "🥑", "🥕", "🌽", "🌶️", "🥦", "🥒", "🍄", "🧄", "🧅", "🧀", "🍠", "🥚", "🥬"];
+
+
+const ul = document.querySelector("#salad-ingredients");
+
+for (let ingredient of ingredients) {
+    const li = document.createElement("li");
+    let btn = document.createElement("button");
+    btn.classList.add("button")
+    btn.innerHTML = ingredient;
+    li.appendChild(btn)
+    ul.appendChild(li)
+}
