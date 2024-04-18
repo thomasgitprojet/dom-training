c9fad02d4a5fed3d329fe12c3d3e41a5f911055b
Thomas Desse <dessethomas@gmail.com>
2024-04-18 14:57:11 +0200

    navigation by tabs 3. 4.

--- js/tabs.js
+++ js/tabs.js
@@ -1,14 +1,16 @@
 const links = document.querySelectorAll(".tabs-link");
-console.log(links);
+const articles = document.querySelectorAll("[data-tab-content]")
 
-
-function addClassActive (element) {
+function addClassActive(element) {
     element.classList.toggle("active")
     console.log(element.dataset.tab);
-
+    let selector = element.dataset.tab;
+    let showArticle = document.querySelector(`#${selector}`);
+    showArticle.classList.toggle("hidden");
+    hiddenArticle(showArticle)
 }
 
-function removeClassActive (array, element) {
+function removeClassActive(array, element) {
     for (data of array) {
         if (!(data.dataset.tab === element.dataset.tab)) {
             data.className = "tabs-link"
@@ -16,9 +18,21 @@ function removeClassActive (array, element) {
     }
 }
 
-for(let onglet of links) {
-    onglet.addEventListener("click", function(e) {
+function hiddenArticle(showArticle) {
+    for (let hidden of articles) {
+        if (!(showArticle.classList === hidden.classList)) {
+            hidden.classList = "tab-content-item hidden"
+        }
+    }
+}
+
+for (let onglet of links) {
+    onglet.addEventListener("click", function (e) {
         addClassActive(onglet)
         removeClassActive(links, onglet)
     })
+}
+
+for (let article of articles) {
+    article.classList.add("hidden");
 }
